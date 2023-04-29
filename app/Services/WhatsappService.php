2e04d94013161d49255613c6744adce3a034da91
baagas0 <baagas0@gmail.com>
2023-04-30 02:17:01 +0700

    fix

--- app/Services/WhatsappService.php
+++ app/Services/WhatsappService.php
@@ -10,7 +10,8 @@ class WhatsappService
 {
     public function http($method, $slug, $data = [], $file = null)
     {
-        $base_uri = 'http://20.231.33.207:3002/';
+        // $base_uri = 'http://20.231.33.207:3002/';
+        $base_uri = 'http://cobain.eastus.cloudapp.azure.com:3002/';
         $slug = $slug . "?key=semnasti-wanya-ketuplak-2";
 
         if ($method == 'get') {
