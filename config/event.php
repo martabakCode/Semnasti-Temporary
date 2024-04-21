7a8323c72c86fd48445d8fd912315b3ce11408d4
baagas0 <baagas0@gmail.com>
2024-04-21 22:10:57 +0700

    Add landing page

--- config/event.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'name' => "SEMNASTI 2023",
-    'date' => "27 s/d 28 Juli 2023",
-    'time' => "08.00 WIB s.d Selesai",
-    'place'=> "Harvard University",
-    'dresscode' => "Casual"
-];
